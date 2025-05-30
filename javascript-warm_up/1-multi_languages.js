5e3d7d28a57b05c38ba3ed93431729a4028c6383
Kelly Hirwa <k.hirwa1@alustudent.com>
2025-05-29 22:51:06 -0700

    k

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,5 @@
+ii#!/usr/bin/node
 #!/usr/bin/node
-const myVar5 = "C is fun"
-const myVar6 = "Python is cool"
-const myVar7 = "JavaScript is amazing"
-console.log(myVar5)
-console.log(myVar6)
-console.log(myVar7) 
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
